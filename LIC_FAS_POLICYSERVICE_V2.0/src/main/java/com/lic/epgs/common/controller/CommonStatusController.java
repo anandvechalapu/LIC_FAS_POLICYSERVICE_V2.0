8bd1720ccb78a882e852944d400ac22c703e69e2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 18:06:07 +0530

    Updated

--- LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/controller/CommonStatusController.java
+++ LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/controller/CommonStatusController.java
@@ -1,36 +1,42 @@
 package com.lic.epgs.common.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.lic.epgs.common.model.CommonStatus;
 import com.lic.epgs.common.service.CommonStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/commonStatus")
+@RequestMapping("/api/commonStatus")
 public class CommonStatusController {
 
-	@Autowired
-	private CommonStatusService commonStatusService;
-	
-	@GetMapping("/{type}")
-	public List<CommonStatus> findByType(@PathVariable String type) {
-		return commonStatusService.findByType(type);
-	}
-	
-	@GetMapping("/active")
-	public List<CommonStatus> findActiveCommonStatuses() {
-		return commonStatusService.findActiveCommonStatuses();
-	}
-	
-	@GetMapping("/{id}")
-	public CommonStatus findById(@PathVariable Long id) {
-		return commonStatusService.findById(id);
-	}
+    @Autowired
+    private CommonStatusService commonStatusService;
+
+    @GetMapping
+    public List<CommonStatus> findAll() {
+        return commonStatusService.findAll();
+    }
+
+    @GetMapping("/type/{type}")
+    public List<CommonStatus> findByType(@PathVariable String type) {
+        return commonStatusService.findByType(type);
+    }
+
+    @GetMapping("/active/{isActive}")
+    public List<CommonStatus> findByIsActive(@PathVariable boolean isActive) {
+        return commonStatusService.findByIsActive(isActive);
+    }
+
+    @GetMapping("/id/{id}")
+    public CommonStatus findById(@PathVariable Long id) {
+        return commonStatusService.findById(id);
+    }
+
+    @GetMapping("/code/{code}")
+    public CommonStatus findByCode(@PathVariable String code) {
+        return commonStatusService.findByCode(code);
+    }
 
 }
