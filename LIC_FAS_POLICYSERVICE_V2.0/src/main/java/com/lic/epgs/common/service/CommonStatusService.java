df88144491809542dd26126bc7506ab025063e2a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 18:05:00 +0530

    Updated

--- LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/service/CommonStatusService.java
+++ LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/service/CommonStatusService.java
@@ -1,40 +1,58 @@
 package com.lic.epgs.common.service;
 
-import com.lic.epgs.common.model.CommonStatus;
-import com.lic.epgs.common.repository.CommonStatusRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lic.epgs.model.CommonStatus;
+import com.lic.epgs.common.repository.CommonStatusRepository;
 
 @Service
 public class CommonStatusService {
 
-    @Autowired
-    private CommonStatusRepository commonStatusRepository;
-
-    //Return a list of all common statuses
-    public List<CommonStatus> findAll() {
-        return commonStatusRepository.findAll();
-    }
-
-    //Return a list of common statuses with a specific type
-    public List<CommonStatus> findByType(String type) {
-        return commonStatusRepository.findByType(type);
-    }
-
-    //Return a list of active common statuses
-    public List<CommonStatus> findByIsActive(boolean isActive) {
-        return commonStatusRepository.findByIsActive(isActive);
-    }
-
-    //Return a common status with a specific id
-    public CommonStatus findById(Long id) {
-        return commonStatusRepository.findById(id);
-    }
-
-    //Return a common status with a specific code
-    public CommonStatus findByCode(String code) {
-        return commonStatusRepository.findByCode(code);
-    }
+	@Autowired
+	CommonStatusRepository commonStatusRepo;
+	
+	// Method to find list of common statuses
+	public List<CommonStatus> findAll(){
+		return commonStatusRepo.findAll();
+	}
+	
+	// Method to find common status by id
+	public Optional<CommonStatus> findById(Long id){
+		return commonStatusRepo.findById(id);
+	}
+	
+	// Method to find common status by code
+	public Optional<CommonStatus> findByCode(String code){
+		return commonStatusRepo.findByCode(code);
+	}
+	
+	// Method to find common status by type
+	public List<CommonStatus> findByType(String type){
+		return commonStatusRepo.findByType(type);
+	}
+	
+	// Method to find common status by description
+	public List<CommonStatus> findByDescription(String description){
+		return commonStatusRepo.findByDescription(description);
+	}
+	
+	// Method to find common status by description1
+	public List<CommonStatus> findByDescription1(String description1){
+		return commonStatusRepo.findByDescription1(description1);
+	}
+	
+	// Method to find common status by name
+	public Optional<CommonStatus> findByName(String name){
+		return commonStatusRepo.findByName(name);
+	}
+	
+	// Method to find common status by isActive
+	public List<CommonStatus> findByIsActive(Boolean isActive){
+		return commonStatusRepo.findByIsActive(isActive);
+	}
+	
 }
