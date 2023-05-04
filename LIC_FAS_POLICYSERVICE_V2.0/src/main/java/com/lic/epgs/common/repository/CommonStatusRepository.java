b56274edf01760ba7e41ca85820561a578e4e80e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 18:01:10 +0530

    Updated

--- LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/repository/CommonStatusRepository.java
+++ LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/repository/CommonStatusRepository.java
@@ -1,37 +1,18 @@
-package com.lic.epgs.common.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.common.model.CommonStatus;
-
 @Repository
 public interface CommonStatusRepository extends JpaRepository<CommonStatus, Long> {
-	
-	/**
-	 * This method returns a list of CommonStatus objects for the given type
-	 * 
-	 * @param type The type of the CommonStatus
-	 * @return A list of CommonStatus objects
-	 */
-	@Query("SELECT cs FROM CommonStatus cs WHERE cs.type = ?1")
-	public List<CommonStatus> findByType(String type);
-	
-	/**
-	 * This method returns a list of CommonStatus objects that are active
-	 * 
-	 * @return A list of CommonStatus objects
-	 */
-	@Query("SELECT cs FROM CommonStatus cs WHERE cs.isActive = true")
-	public List<CommonStatus> findActiveCommonStatuses();
-	
-	/**
-	 * This method returns a CommonStatus object for the given id
-	 * 
-	 * @param id The id of the CommonStatus
-	 * @return The CommonStatus object
-	 */
-	public CommonStatus findById(Long id);
 
+    //Return a list of all common statuses
+    List<CommonStatus> findAll();
+
+    //Return a list of common statuses with a specific type
+    List<CommonStatus> findByType(String type);
+
+    //Return a list of active common statuses
+    List<CommonStatus> findByIsActive(boolean isActive);
+
+    //Return a common status with a specific id
+    CommonStatus findById(Long id);
+
+    //Return a common status with a specific code
+    CommonStatus findByCode(String code);
 }
