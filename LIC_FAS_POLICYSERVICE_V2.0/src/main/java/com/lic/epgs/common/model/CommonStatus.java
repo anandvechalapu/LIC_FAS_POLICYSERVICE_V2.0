b3a653dc8410192a821e7bfb162b9b63bd8e50e1
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 18:02:35 +0530

    Updated

--- LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/model/CommonStatus.java
+++ LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/model/CommonStatus.java
@@ -1,76 +1,52 @@
+//Entity/Model class
 package com.lic.epgs.common.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-@Table(name = "common_status")
 public class CommonStatus {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String code;
+    private String type;
+    private boolean isActive;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name = "type")
-	private String type;
-	
-	@Column(name = "status_code")
-	private String statusCode;
-	
-	@Column(name = "status")
-	private String status;
-	
-	@Column(name = "is_active")
-	private boolean isActive;
-	
-	public CommonStatus() {
-		
-	}
-	
-	public CommonStatus(String type, String statusCode, String status, boolean isActive) {
-		this.type = type;
-		this.statusCode = statusCode;
-		this.status = status;
-		this.isActive = isActive;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getType() {
-		return type;
-	}
-	
-	public void setType(String type) {
-		this.type = type;
-	}
-	
-	public String getStatusCode() {
-		return statusCode;
-	}
-	
-	public void setStatusCode(String statusCode) {
-		this.statusCode = statusCode;
-	}
-	
-	public String getStatus() {
-		return status;
-	}
-	
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	
-	public boolean isActive() {
-		return isActive;
-	}
-	
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
-	
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
 }
