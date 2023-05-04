bc017610a14d058bc6c5d33ead5871f3bce70232
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 18:04:27 +0530

    Updated

--- LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/service/CommonStatusService.java
+++ LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/service/CommonStatusService.java
@@ -1,29 +1,40 @@
 package com.lic.epgs.common.service;
 
-import java.util.List;
-
+import com.lic.epgs.common.model.CommonStatus;
+import com.lic.epgs.common.repository.CommonStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.common.model.CommonStatus;
-import com.lic.epgs.common.repository.CommonStatusRepository;
+import java.util.List;
 
 @Service
 public class CommonStatusService {
 
-	@Autowired
-	private CommonStatusRepository commonStatusRepository;
+    @Autowired
+    private CommonStatusRepository commonStatusRepository;
 
-	public List<CommonStatus> findByType(String type) {
-		return commonStatusRepository.findByType(type);
-	}
-	
-	public List<CommonStatus> findActiveCommonStatuses() {
-		return commonStatusRepository.findActiveCommonStatuses();
-	}
-	
-	public CommonStatus findById(Long id) {
-		return commonStatusRepository.findById(id);
-	}
+    //Return a list of all common statuses
+    public List<CommonStatus> findAll() {
+        return commonStatusRepository.findAll();
+    }
 
+    //Return a list of common statuses with a specific type
+    public List<CommonStatus> findByType(String type) {
+        return commonStatusRepository.findByType(type);
+    }
+
+    //Return a list of active common statuses
+    public List<CommonStatus> findByIsActive(boolean isActive) {
+        return commonStatusRepository.findByIsActive(isActive);
+    }
+
+    //Return a common status with a specific id
+    public CommonStatus findById(Long id) {
+        return commonStatusRepository.findById(id);
+    }
+
+    //Return a common status with a specific code
+    public CommonStatus findByCode(String code) {
+        return commonStatusRepository.findByCode(code);
+    }
 }
