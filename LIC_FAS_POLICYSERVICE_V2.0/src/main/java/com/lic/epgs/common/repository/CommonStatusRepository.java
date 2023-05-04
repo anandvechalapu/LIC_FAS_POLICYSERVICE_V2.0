3c70b609aa11e1d4d267fb6b949f9c92228175c8
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 18:01:40 +0530

    Updated

--- LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/repository/CommonStatusRepository.java
+++ LIC_FAS_POLICYSERVICE_V2.0/src/main/java/com/lic/epgs/common/repository/CommonStatusRepository.java
@@ -1,18 +1,35 @@
+package com.lic.epgs.common.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lic.epgs.model.CommonStatus;
+
 @Repository
 public interface CommonStatusRepository extends JpaRepository<CommonStatus, Long> {
-
-    //Return a list of all common statuses
-    List<CommonStatus> findAll();
-
-    //Return a list of common statuses with a specific type
-    List<CommonStatus> findByType(String type);
-
-    //Return a list of active common statuses
-    List<CommonStatus> findByIsActive(boolean isActive);
-
-    //Return a common status with a specific id
-    CommonStatus findById(Long id);
-
-    //Return a common status with a specific code
-    CommonStatus findByCode(String code);
+	
+	// Method to find list of common statuses
+	public List<CommonStatus> findAll();
+	
+	// Method to find common status by id
+	public Optional<CommonStatus> findById(Long id);
+	
+	// Method to find common status by code
+	public Optional<CommonStatus> findByCode(String code);
+	
+	// Method to find common status by type
+	public List<CommonStatus> findByType(String type);
+	
+	// Method to find common status by description
+	public List<CommonStatus> findByDescription(String description);
+	
+	// Method to find common status by description1
+	public List<CommonStatus> findByDescription1(String description1);
+	
+	// Method to find common status by name
+	public Optional<CommonStatus> findByName(String name);
+	
+	// Method to find common status by isActive
+	public List<CommonStatus> findByIsActive(Boolean isActive);
+	
 }
